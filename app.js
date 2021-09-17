3e8807c0d0caeef66f8c17c6e334627dcd8ba372
Madhin <madhinhussain@gmail.com>
2021-09-17 16:37:38 +0100

    styling updated

--- app.js
+++ app.js
@@ -10,3 +10,46 @@ const productPrice = productDataWomens.map((product) => {
 
 console.log(productImage[0]);
 console.log(productPrice[0]);
+
+
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    centerMode: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+
+      }
+    }, {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+
+      }
+    },  {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      }
+    }]
+  });
+});
+</script>
