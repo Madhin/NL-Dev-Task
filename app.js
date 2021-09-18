dba0f1e00a80359aa56d547e64344e627369ac24
Madhin <madhinhussain@gmail.com>
2021-09-18 22:47:16 +0100

    range slider added

--- app.js
+++ app.js
@@ -1,98 +1,74 @@
 var productDataWomens = [{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-grey-faux-fur-longline-coat/p/619712403', imageSrc: 'https://media2.newlookassets.com/i/newlook/619712403.jpg', productTitle: 'Dark Grey Faux Fur Longline Coat', price: '45.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/tops/black-ribbed-long-sleeve-roll-neck-top/p/635105501', imageSrc: 'https://media2.newlookassets.com/i/newlook/635105501.jpg', productTitle: 'Black Ribbed Long Sleeve Roll Neck Top', price: '8.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/leggings/black-coated-leather-look-leggings/p/634018201', imageSrc: 'https://media3.newlookassets.com/i/newlook/634018201.jpg', productTitle: 'Black Coated Leather-Look Leggings', price: '19.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-faux-fur-longline-coat/p/619712401', imageSrc: 'https://media3.newlookassets.com/i/newlook/619712401.jpg', productTitle: 'Black Faux Fur Longline Coat', price: '45.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-high-waist-super-skinny-hallie-jeans/p/635654901', imageSrc: 'https://media2.newlookassets.com/i/newlook/635654901.jpg', productTitle: 'Black High Waist Super Skinny Hallie Jeans', price: '25.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/camel-revere-collar-coat/p/619653214', imageSrc: 'https://media2.newlookassets.com/i/newlook/619653214.jpg', productTitle: 'Camel Revere Collar Coat', price: '35.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-jeggings/p/623140201', imageSrc: 'https://media3.newlookassets.com/i/newlook/623140201.jpg', productTitle: 'Black Lift & Shape Jeggings', price: '17.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-aviator-jacket/p/632207701', imageSrc: 'https://media2.newlookassets.com/i/newlook/632207701.jpg', productTitle: 'Black Leather-Look Aviator Jacket', price: '55.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/light-grey-fluffy-houndstooth-double-breasted-coat-/p/631197208', imageSrc: 'https://media3.newlookassets.com/i/newlook/631197208.jpg', productTitle: 'Light Grey Fluffy Houndstooth Double Breasted Coat', price: '49.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/leggings/black-high-waist-leggings/p/616323901', imageSrc: 'https://media3.newlookassets.com/i/newlook/616323901.jpg', productTitle: 'Black High Waist Leggings', price: '8.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-coated-super-skinny-hallie-jeans/p/631910801', imageSrc: 'https://media2.newlookassets.com/i/newlook/631910801.jpg', productTitle: 'Black Coated Super Skinny Hallie Jeans', price: '27.99'},{ productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/black-leather-look-chunky-lace-up-boots/p/629029201', imageSrc: 'https://media3.newlookassets.com/i/newlook/629029201.jpg', productTitle: 'Black Leather-Look Chunky Lace Up Boots', price: '27.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-high-waist-skinny-jeans/p/631911801', imageSrc: 'https://media3.newlookassets.com/i/newlook/631911801.jpg', productTitle: 'Black Lift & Shape High Waist Skinny Jeans', price: '25.99'},{ productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/wide-fit-black-teddy-lined-chunky-lace-up-boots/p/637716701', imageSrc: 'https://media3.newlookassets.com/i/newlook/637716701.jpg', productTitle: 'Wide Fit Black Teddy Lined Chunky Lace Up Boots', price: '27.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-green-faux-fur-longline-coat/p/619712438', imageSrc: 'https://media3.newlookassets.com/i/newlook/619712438.jpg', productTitle: 'Dark Green Faux Fur Longline Coat', price: '45.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/nightwear/pale-grey-fluffy-hooded-dressing-gown/p/629877202', imageSrc: 'https://media3.newlookassets.com/i/newlook/629877202.jpg', productTitle: 'Pale Grey Fluffy Hooded Dressing Gown', price: '25.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/pale-grey-revere-collar-coat/p/619653202', imageSrc: 'https://media2.newlookassets.com/i/newlook/619653202.jpg', productTitle: 'Pale Grey Revere Collar Coat', price: '35.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-super-soft-super-skinny-india-jeans/p/623185401', imageSrc: 'https://media2.newlookassets.com/i/newlook/623185401.jpg', productTitle: 'Black Super Soft Super Skinny India Jeans', price: '22.99'},{ productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-biker-jacket-/p/620187701', imageSrc: 'https://media2.newlookassets.com/i/newlook/620187701.jpg', productTitle: 'Black Leather-Look Biker Jacket', price: '32.99'},{ productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/black-leather-flat-chelsea-boots/p/633518801', imageSrc: 'https://media2.newlookassets.com/i/newlook/633518801.jpg', productTitle: 'Black Leather Flat Chelsea Boots', price: '35.99'},];
 
 const productImage = productDataWomens.map((product) => {
-  return product.imageSrc
+  return `
+  <div class="card">
+  <div class="card-header">
+    <img src="${product.imageSrc}">
+  </div>
+  <div class="card-body">
+    <div class="card-content">
+      <div class="card-title">${product.productTitle}</div>
+      <div class="card-text">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+    </div>
+  </div>
+</div>`
+});
+
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    centerMode: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+
+      }
+
+    }, {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+
+      }
+    },  {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      }
+    }]
+  });
 });
 
 const productPrice = productDataWomens.map((product) => {
   return product.price
 });
 
-console.log(productImage[0]);
-console.log(productPrice[0]);
-
-window.addEventListener('DOMContentLoaded', () => {
-	new dualRangeSlider(document.querySelector(".dual-range"))
-})
+console.log(productImage);
 
 
 
 
 
 
-class dualRangeSlider {
-	constructor(rangeElement) {
-		this.range = rangeElement
-		this.min = Number(rangeElement.dataset.min)
-		this.max = Number(rangeElement.dataset.max)
-		this.handles = [...this.range.querySelectorAll(".handle")]
-		this.startPos = 0;
-		this.activeHandle;
-		
-		this.handles.forEach(handle => {
-			handle.addEventListener("mousedown", this.startMove.bind(this))
-			handle.addEventListener("touchstart", this.startMoveTouch.bind(this))
-		})
-		
-		window.addEventListener("mouseup", this.stopMove.bind(this))
-		window.addEventListener("touchend", this.stopMove.bind(this))
-		window.addEventListener("touchcancel", this.stopMove.bind(this))
-		window.addEventListener("touchleave", this.stopMove.bind(this))
-		
-		const rangeRect = this.range.getBoundingClientRect();
-		const handleRect = this.handles[0].getBoundingClientRect()
-		this.range.style.setProperty("--x-1", "0px");
-		this.range.style.setProperty("--x-2", rangeRect.width - handleRect.width/2 + "px");
-		this.handles[0].dataset.value = this.range.dataset.min;
-		this.handles[1].dataset.value = this.range.dataset.max;
-	}
-	
-	startMoveTouch(e) {
-		const handleRect = e.target.getBoundingClientRect()
-		this.startPos = e.touches[0].clientX - handleRect.x;
-		this.activeHandle = e.target;
-		this.moveTouchListener = this.moveTouch.bind(this)
-		window.addEventListener("touchmove", this.moveTouchListener);
-	}
-	
-	startMove(e) {
-		this.startPos = e.offsetX;
-		this.activeHandle = e.target;
-		this.moveListener = this.move.bind(this)
-		window.addEventListener("mousemove", this.moveListener);
-	}
-	
-	moveTouch(e) {
-		this.move({clientX: e.touches[0].clientX})
-	}
-	
-	move(e) {
-		const isLeft = this.activeHandle.classList.contains("left")
-		const property = isLeft ? "--x-1" : "--x-2";
-		const parentRect = this.range.getBoundingClientRect();
-		const handleRect = this.activeHandle.getBoundingClientRect();
-		let newX = e.clientX - parentRect.x - this.startPos;
-		if(isLeft) {
-			const otherX = parseInt(this.range.style.getPropertyValue("--x-2"));
-			newX = Math.min(newX, otherX - handleRect.width)
-			newX = Math.max(newX, 0 - handleRect.width/2)
-		} else {
-			const otherX = parseInt(this.range.style.getPropertyValue("--x-1"));
-			newX = Math.max(newX, otherX + handleRect.width)
-			newX = Math.min(newX, parentRect.width - handleRect.width/2)
-		}
-		this.activeHandle.dataset.value = this.calcHandleValue((newX + handleRect.width/2) / parentRect.width)
-		this.range.style.setProperty(property, newX + "px");
 
-	}
-	
-	calcHandleValue(percentage) {
-		return Math.round(percentage * (this.max - this.min) + this.min)
-	}
-	
-	stopMove() {
-		window.removeEventListener("mousemove", this.moveListener);
-		window.removeEventListener("touchmove", this.moveTouchListener);
-	}
-}
 
